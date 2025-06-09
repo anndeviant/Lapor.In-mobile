6d4109d5f0e5ed175d95d9186e834ec16cf48c4b
Annas Sovianto <141797683+Reigentsu@users.noreply.github.com>
2025-06-09 12:03:50 +0700

    update db name

--- laporin_be/config/database.js
+++ laporin_be/config/database.js
@@ -3,8 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
-const DB_NAME = process.env.DB_NAME;
+const DB_NAME = "laporin_mobile";
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
